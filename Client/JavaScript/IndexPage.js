e89a8d8c5b643527ebe7a6626fa03748fdede5cb
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-04-27 22:08:18 +0530

    Added Website Index

---
+++ Client/JavaScript/IndexPage.js
@@ -0,0 +1,15 @@
+// Reload Function
+function reloadPage()
+{
+    window.location.reload();
+}
+
+// Hamburger Menu
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
