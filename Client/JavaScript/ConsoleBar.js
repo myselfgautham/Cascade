9512097649a92de5dcfe3680dcbfb0dfe8feb61f
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-04-30 17:26:13 +0530

    Added Console Changes

--- Client/JavaScript/ConsoleBar.js
+++ Client/JavaScript/ConsoleBar.js
@@ -12,3 +12,12 @@ function closeNav()
         document.getElementById("cls").className = "closebtn";
     },400)
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const negativeValue = [null,"null",false,"false"];
+    const cookiesAccepted = localStorage.getItem("cookiesAccepted");
+    if (negativeValue.includes(cookiesAccepted))
+    {
+        window.location.href = "/";
+    }
+})
