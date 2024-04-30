84370b663595d0c61ae998402afab2772bb20378
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-04-30 09:47:19 +0530

    Deleted Code

--- Server/__init__.py
+++
@@ -1,8 +0,0 @@
-# Server Software Metadata
-METADATA: dict[str] = {
-    "Name" : "Simple Server Software",
-    "Version" : "1.0",
-    "Version Type" : "Beta",
-    "Developed By" : "Gautham Krishna V",
-    "GitHub ID" : "@myselfgautham"
-}
