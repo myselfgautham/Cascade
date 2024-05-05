334ee939a3ec4163865566536a38f1fee9c5a819
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-05-05 21:51:13 +0530

    Added Verification Check

--- Server/PyrebaseSDK.py
+++ Server/PyrebaseSDK.py
@@ -1,4 +1,5 @@
 from pyrebase import pyrebase
+from firebase_admin.auth import get_user_by_email
 
 firebaseConfig: dict[str,str] = {
   "apiKey": "AIzaSyBIpNPJ1jYxuHmAlEJVeQEaFuWz2wofONc",
@@ -11,15 +12,26 @@ firebaseConfig: dict[str,str] = {
   "databaseURL": ""
 }
 
+class UserNotVerified(Exception):
+    message = "Account Not Verified"
+    def __init__(self) -> None:
+        super().__init__(self.message)
+        return None
+
 application = pyrebase.initialize_app(firebaseConfig)
 authentication = application.auth()
 
 def loginUserWithEmailAndPassword(email: str, password: str):
+    user = get_user_by_email(email=email)
     response = {}
     try:
         _ = authentication.sign_in_with_email_and_password(email=email, password=password)
+        if not user.email_verified:
+            raise UserNotVerified()
         response["response"] = "Sign In Completed Successfully"
     except pyrebase.HTTPError:
         response['response'] = "Invalid Credentials"
+    except UserNotVerified:
+        response["response"] = UserNotVerified.message
     finally:
         return response
