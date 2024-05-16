f49127beb93a8d77208d94ebac71cc845156da00
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-05-16 13:25:22 +0530

    Added An Application
    Used WebView On Android Studio Starting From Android 7.0 Runs On Almost 96% Of Devices (Android) Worldwide

---
+++ Android/app/src/main/java/com/simple/cards/MainActivity.java
@@ -0,0 +1,30 @@
+package com.simple.cards;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import android.webkit.WebSettings;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    WebView buffer;
+    String WebSiteAddress = "http://192.168.43.189:1920";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buffer = findViewById(R.id.Website);
+        WebSettings settings = buffer.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        buffer.setWebViewClient(new WebViewClient());
+        buffer.setScrollContainer(false);
+        buffer.loadUrl(WebSiteAddress);
+    }
+}
