ffb46371316432da5ad8774272c56665c21c178b
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-05-26 20:42:19 +0530

    Minor Fixes

--- Client/JavaScript/Cookies.js
+++ Client/JavaScript/Cookies.js
@@ -1,9 +1,9 @@
 let overlay = document.getElementById("overlay");
-if (localStorage.getItem("cookiesAccepted") === null) {
+if (localStorage.getItem("cookiesAccepted") !== "true") {
     overlay.style.display = "flex";
     let accept = document.getElementById("accept");
     accept.addEventListener("click", () => {
-        localStorage.setItem("cookiesAccepted", true);
+        localStorage.setItem("cookiesAccepted", "true");
         setTimeout(() => {
             window.location.reload();
         }, 400);
