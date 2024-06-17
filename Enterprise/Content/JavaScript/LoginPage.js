2919eee04a3b57a284782dafb18e08062270a46d
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-06-17 16:35:36 +0530

    Requests

--- Enterprise/Content/JavaScript/LoginPage.js
+++ Enterprise/Content/JavaScript/LoginPage.js
@@ -2,3 +2,20 @@ let register = document.getElementById("register");
 register.addEventListener("click", () => {
     window.location.href = "/account/register";
 })
+
+let submit = document.getElementById("submit");
+submit.addEventListener("click", () => {
+    fetch("/account/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {})
+    .catch(error => console.error("Error : ", error))
+})
--- Enterprise/Content/JavaScript/RegisterPage.js
+++ Enterprise/Content/JavaScript/RegisterPage.js
@@ -2,3 +2,23 @@ let login = document.getElementById("login");
 login.addEventListener("click", () => {
     window.location.href = "/account/login"
 })
+
+let submit = document.getElementById("submit");
+submit.addEventListener("click", () => {
+    fetch("/account/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            business: document.getElementById("name").value,
+            owner: document.getElementById("owner").value,
+            email: document.getElementById("email").value,
+            phone: document.getElementById("phone").value,
+            password: document.getElementById("password").value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {})
+    .catch(error => console.error("Error : ", error))
+})
