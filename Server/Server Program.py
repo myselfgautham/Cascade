f1470a356c6ae123676fc4f0131f72c26eae8b5a
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-06-23 12:55:55 +0530

    Adding Nodes

--- Server/Server Program.py
+++ Server/Server Program.py
@@ -240,11 +240,11 @@ def shareCardUIAndEndpoint():
         return render_template("ShareCard.html")
     else:
         try:
-            get_user_by_email(request.json.get("email"))
+            get_user_by_email(request.json.get("party"))
             if not (checkDeviceAuthorization(request.json.get("uid"), request.json.get("email"))):
                 return jsonify({"Response": "Unauthorized Device"})
             db.collection("Cards").document(request.json.get("card")).update({
-                "Owners": firestore.ArrayUnion([request.json.get("email")])
+                "Owners": firestore.ArrayUnion([request.json.get("party")])
             })
             return jsonify({"Response": "Added"})
         except UserNotFoundError:
@@ -269,3 +269,25 @@ def returnSystemWideCPUUsage():
 @application.errorhandler(404)
 def serve404ErrorPage(_):
     return render_template("PageNotFound.html")
+
+# Nodes Manager Page
+@application.route("/user/nodes", methods = ["GET", "POST"])
+def serveNodesManagerPage():
+    if request.method == "GET":
+        return render_template("NodesPage.html")
+    else:
+        try:
+            data: dict = request.json
+            if not (checkDeviceAuthorization(data.get("uid"), data.get("email"))):
+                return jsonify({"Response": "Unauthorized Device"})
+            returnData: dict = dict()
+            dataFetched = db.collection("Nodes").where(filter=FieldFilter(
+                field_path="`User Email`",
+                op_string="==",
+                value=data.get("email")
+            )).stream()
+            for document in dataFetched:
+                returnData[document.id] = document.to_dict()
+            return jsonify({"Response": returnData})
+        except Exception:
+            return jsonify({"Response": "Error"})
