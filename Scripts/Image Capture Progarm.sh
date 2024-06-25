e2c2cba659d6fde2a5029df998ca90c583a295f7
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-06-25 18:26:10 +0530

    Changes

--- Scripts/Image Capture Progarm.sh
+++ Scripts/Image Capture Progarm.sh
@@ -3,5 +3,5 @@
 PGM_NAME="Image_Capture_Program_jar"
 JAR_NAME="Image Capture Program.jar"
 
-cd "../Java/out/artifacts/$PGM_NAME" ||exit 1
+cd "../Java/Output/artifacts/$PGM_NAME" ||exit 1
 java -jar "$JAR_NAME"
