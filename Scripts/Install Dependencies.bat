8065f9d895bb692fab23f51db4f6064a68a56a68
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-07-30 07:33:45 +0530

    Adding Windows Scripts

---
+++ Scripts/Install Dependencies.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+CLS
+python -m pip install -r ./Requirements.txt
+PAUSE
+CLS
--- Scripts/Install Dependencies.sh
+++ Scripts/Install Dependencies.sh
@@ -3,5 +3,6 @@ printf "\nInstalling Python Dependencies\n\n"
 sleep 1
 clear
 python3.12 -m pip install -r ./Requirements.txt
+python3.12 -m pip install gunicorn
 sleep 1
 clear
