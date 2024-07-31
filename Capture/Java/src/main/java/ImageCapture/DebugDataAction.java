d4070f7303e34a1b5ac97f1ef2bd679b43a97821
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-07-31 08:54:55 +0530

    Java Image Capture Program

---
+++ Capture/Java/src/main/java/ImageCapture/DebugDataAction.java
@@ -0,0 +1,24 @@
+package ImageCapture;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class DebugDataAction
+{
+    private ImageCapture imageCapture;
+    private byte[] commandData;
+    private int commandDataLength;
+
+    public DebugDataAction(ImageCapture imageCapture, byte[] commandData, int commandDataLength) {
+        this.imageCapture = imageCapture;
+        this.commandData = commandData;
+        this.commandDataLength = commandDataLength;
+    }
+
+    public void process() {
+        if (commandDataLength > 0) {
+            String debugText = new String(Arrays.copyOfRange(commandData, 1, commandDataLength), StandardCharsets.UTF_8);
+            imageCapture.printDebugData(debugText);
+        }
+    }
+}
