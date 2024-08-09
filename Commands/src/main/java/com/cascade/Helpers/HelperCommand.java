a2f9d54112875c116392ce1feec1914acb8c17b9
Gautham Krishna Vinayachandran <gauthamkrishnav@icloud.com>
2024-08-09 21:28:10 +0530

    Added Stuff

--- Commands/src/main/java/com/cascade/Helpers/HelperCommand.java
+++ Commands/src/main/java/com/cascade/Helpers/HelperCommand.java
@@ -4,17 +4,33 @@ import java.util.HashMap;
 
 public class HelperCommand {
     protected static HashMap<String, String> CommandsDocumentation = new HashMap<>();
+    protected static HashMap<String, String> FlagsDocumentation = new HashMap<>();
 
     public static void RegisterCommands() {
         CommandsDocumentation.put("clear", "Clears The Screen / Terminal");
         CommandsDocumentation.put("exit", "Exits The Shell Session");
         CommandsDocumentation.put("help", "Prints The Documentation Of Available Commands");
+        RegisterFlags();
+    }
+
+    private static void RegisterFlags() {
+        FlagsDocumentation.put("-verbose", "Added Stack Trace Logging");
     }
 
     public static void PrintHelpCommandsData() {
         System.out.println();
+        System.out.println("\u001B[34mCommands\u001B[0m :=");
+        System.out.println();
         for (String key : CommandsDocumentation.keySet()) {
+            System.out.print("    ");
             System.out.println(key + " => " + CommandsDocumentation.get(key));
+        }
+        System.out.println();
+        System.out.println("\u001B[31mFlags\u001B[0m :=");
+        System.out.println();
+        for(String key : FlagsDocumentation.keySet()) {
+            System.out.print("    ");
+            System.out.println(key + " => " + FlagsDocumentation.get(key));
         }
         System.out.println();
     }
